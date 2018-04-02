785d7b04ae388d3eafb070003d7f9d7053eb640c
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-02 23:11:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,7 @@
 def fizzbuzz(input)
-  if input%3 == 0 && input%5 == 0
+  if input%3 == 0 && input%5 == 0 && input%10 ==0
+    return "Doormamu"
+  elsif input%3 == 0 && input%5 == 0
     return "FizzBuzz"
   elsif input%5 ==0
     return "Buzz"
